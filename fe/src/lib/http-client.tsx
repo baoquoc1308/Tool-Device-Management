47af4d056dc74713285d92a67207e646e2f1205f
Quoc Dinh <dinhanhquoc2525@gmail.com>
2025-05-19 17:08:31 +0700

    fix: call api refresh once

--- fe/src/lib/http-client.tsx
+++ fe/src/lib/http-client.tsx
@@ -1,6 +1,16 @@
 import { httpRequest, tryCatch } from '@/utils'
-import axios from 'axios'
+import axios, { type InternalAxiosRequestConfig } from 'axios'
 import Cookies from 'js-cookie'
+
+let isRefreshing = false
+
+type RequestFailed = {
+  config: InternalAxiosRequestConfig
+  resolve: (value: any) => void
+  reject: (reason?: any) => void
+}
+
+let requestFailed: RequestFailed[] = []
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -33,22 +43,37 @@ httpClient.interceptors.response.use(
     const status = error.response?.data.status
     const message = error.response?.data.msg
     if ((status === 401 && message === 'Access Token expired') || message === 'Access Token Invoked') {
-      const { data, error } = await tryCatch(httpRequest.post('/auth/refresh', { refresh_token: refreshToken }))
-      if (error) {
-        if (
-          (error as any).response?.data.msg === 'Refresh token was expired' ||
-          (error as any).response?.data.msg === 'Refresh token was invoked'
-        ) {
-          Cookies.remove('accessToken')
-          Cookies.remove('refreshToken')
-          window.location.href = '/login'
-          return Promise.reject(error)
-        } else return Promise.reject(error)
+      if (!isRefreshing) {
+        isRefreshing = true
+        const { data, error } = await tryCatch(httpRequest.post('/auth/refresh', { refresh_token: refreshToken }))
+        if (error) {
+          if (
+            (error as any).response?.data.msg === 'Refresh token was expired' ||
+            (error as any).response?.data.msg === 'Refresh token was invoked'
+          ) {
+            Cookies.remove('accessToken')
+            Cookies.remove('refreshToken')
+            window.location.href = '/login'
+            return Promise.reject(error)
+          } else return Promise.reject(error)
+        }
+        Cookies.set('accessToken', data?.data?.data.access_token)
+        config.headers.Authorization = `Bearer ${data?.data?.data.access_token}`
+        for (const { config, resolve, reject } of requestFailed) {
+          try {
+            const data = await httpClient(config)
+            resolve(data)
+          } catch (error) {
+            reject(error)
+          }
+        }
+        requestFailed = []
+        isRefreshing = false
+        return await httpClient(config)
       }
-      Cookies.set('accessToken', data?.data?.data.access_token)
-      config.headers.Authorization = `Bearer ${data?.data?.data.access_token}`
-
-      return await httpClient(config)
+      return new Promise((resolve, reject) => {
+        requestFailed.push({ config, resolve, reject })
+      })
     }
     return Promise.reject(error)
   }
