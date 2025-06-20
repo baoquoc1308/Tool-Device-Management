74d45319280723022f1b7f0ae7a1aa8be15245b7
Rangdog <thanhhaxuan02@gmail.com>
2025-06-20 15:53:52 +0700

    fix issue transfer req

--- be/internal/repository/maintenance_schedules/impl_interface.go
+++ be/internal/repository/maintenance_schedules/impl_interface.go
@@ -24,7 +24,7 @@ func (r *PostgreSQLMaintenanceSchedulesRepository) GetAllMaintenanceSchedulesByA
 	maintenances := []*entity.MaintenanceSchedules{}
 	startOfDay := time.Now().Truncate(24 * time.Hour)
 	endOfDay := startOfDay.Add(24 * time.Hour)
-	result := r.db.Model(entity.MaintenanceSchedules{}).Where("asset_id = ?", assetId).Where("end_date >= ?", endOfDay).Preload("Asset").Find(&maintenances)
+	result := r.db.Model(entity.MaintenanceSchedules{}).Joins("join assets on assets.id = maintenance_schedules.asset_id").Where("maintenance_schedules.asset_id = ?", assetId).Where("maintenance_schedules.end_date >= ?", endOfDay).Where("assets.status != ? and assets.status != ?","Disposed","Retired").Preload("Asset").Find(&maintenances)
 	if result.Error != nil {
 		return nil, result.Error
 	}
