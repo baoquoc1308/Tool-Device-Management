7c64f9eb973f4b0eb94f4cb2ee79b3ed1cecd5d1
Quoc Dinh <dinhanhquoc2525@gmail.com>
2025-06-23 15:31:44 +0700

    fix: some issues

--- fe/src/features/auth/slice/auth-action.tsx
+++ fe/src/features/auth/slice/auth-action.tsx
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import { tryCatch } from '@/utils'
 import { httpRequest } from '@/utils'
 import type { AxiosError } from 'axios'
+import { toast } from 'sonner'
 
 // create async thunk to handle when user log in
 type DataLogInType = {
