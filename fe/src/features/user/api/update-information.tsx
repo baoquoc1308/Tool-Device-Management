6aa06559d9635e5902004d47ad16ee330c6bce33
Ho Bao Quoc <137763297+baoquoc1308@users.noreply.github.com>
2025-06-26 10:46:49 +0700

    Revert "Avatar user"

--- fe/src/features/user/api/update-information.tsx
+++
@@ -1,7 +0,0 @@
-import { httpRequest } from '@/utils'
-import type { UpdateInformationFormType } from '../model/update-information-schema'
-
-export const updateInformation = async (value: UpdateInformationFormType) => {
-  console.log('🚀 ~ updateInformation ~ value:', value)
-  return await httpRequest.patch('/user/information', { ...value }, { 'Content-Type': 'multipart/form-data' })
-}
